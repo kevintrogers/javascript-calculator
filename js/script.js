70a6e8eb459b5652cbd19d80aac71d96ffa297f2
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-06-22 16:14:06 +0000

    updated files to codepen versions

--- js/script.js
+++ js/script.js
@@ -1,147 +1,109 @@
 $(document).ready(function() {
-//1-15-2017 calculator working RPN style
+
   let subtract, add, divide, multiply, calculate, percentage, clear, allClear, plusMinus, temp = '',
-    setOperation,  operation_arr = ['+','-','x','/','='], firstNum = 0, secondNum = 0, total, display = $('#display'), displayValue, storedOperator, performOperation;
+    firstNum = 0,
+    secondNum = 0,
+    total, displayValue, storedOperator, performOperation;
 
   subtract = function(num1, num2) {
     total = Number(num1) - Number(num2);
     return total;
   };
-  
+
   add = function(num1, num2) {
     total = Number(num1) + Number(num2);
     return total;
   };
-  
+
   multiply = function(num1, num2) {
     total = (Number(num1)) * (Number(num2));
     return total;
   };
-  
+
   divide = function(num1, num2) {
     total = (Number(num1)) / (Number(num2));
     return total;
   };
-  
+
   plusMinus = function() {
     if (total === 0) {
-    total = Number(temp) * -1;
-    } else {
+      total = Number(temp) * -1;
+    }
+    else {
       total = Number(total) * -1;
     }
-    
-   };
-  
+
+  };
+
   clear = function() {
     total = 0;
     temp = 0;
     displayValue(total);
   };
-  
+
   allClear = function() {
     total = 0;
     firstNum = 0;
     secondNum = 0;
     displayValue(total);
   };
-  
-   display.keypress(function(e) {
-     //if value !num check for existence num 1 
-     //store operation value
-     //if no num display val is num 1 else is num 2
-     //perform operation
-             if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-             // Allow: Ctrl/cmd+A
-            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+C
-            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+X
-            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
-        }
- 
-      if (temp === 0 ) {
-  
-        temp = $(this).val().toString();
-  
-      } else  {
-    
-        temp += $(this).val().toString();
-  
-      }
-   
-    
-    });
-  
+
   $('.int').click(function() {
-    
+
     if (temp === 0) {
       temp = $(this).val().toString();
       displayValue(temp);
-    } else {
+    }
+    else {
       temp += $(this).val().toString();
       displayValue(temp);
     }
-  }); 
-  
- //display.keypress()
-  
-  $('.operation').click(function() {
-    setOperation();
   });
-  
-  function setOperation() {
+
+  $('.operation').click(function() {
+
     operation = $(this).val();
     if (firstNum === 0) {
-    firstNum = temp;
-    displayValue(temp);
-    temp = 0;
-    storedOperator = operation;
-    } else {
+      firstNum = temp;
+      displayValue(temp);
+      temp = 0;
+      storedOperator = operation;
+    }
+    else {
       secondNum = temp;
       performOperation(firstNum, secondNum, storedOperator);
       displayValue(total);
       storedOperator = operation;
     }
-  
-  }
-  
-  $('.imm-op').click(function() {
-    immOperation();
+
+
   });
-  
-  function immOperation() {
+  $('.imm-op').click(function() {
     operation = $(this).val();
     performOperation(firstNum, secondNum, operation);
-  }
-  
+  });
   performOperation = function(a, b, operation_type) {
     switch (operation_type) {
-
-      case '-':
-        subtract(a,b);
-        break;
       case '+':
-        total = add(a,b);
+        total = add(a, b);
+        break;
+      case '-':
+        subtract(a, b);
         break;
       case '/':
-        divide(a,b);
+        divide(a, b);
         break;
       case 'x':
-        multiply(a,b);
+        multiply(a, b);
         break;
       case 'C':
         clear();
         break;
       case 'AC':
         allClear();
+        break;
+      case '%':
+        percentage();
         break;
       case 'plus_minus':
         plusMinus(temp);
@@ -154,9 +116,9 @@ $(document).ready(function() {
     temp = 0;
     firstNum = total;
   }
-  
+
   displayValue = function(value) {
-    display.val(value);
+    $('#display').val(value);
 
   };
 
