8d30c670fbefea7e69ab050ec200c1af36a26b6a
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-06-23 03:18:53 +0000

    finished reverting script.js

--- js/script.js
+++ js/script.js
@@ -49,6 +49,7 @@ $(document).ready(function() {
     displayValue(total);
   };
 
+
   display.keypress(function(e) {
     //if value !num check for existence num 1 
     //store operation value
@@ -85,6 +86,7 @@ $(document).ready(function() {
 
   });
 
+
   $('.int').click(function() {
 
     if (temp === 0) {
@@ -104,6 +106,8 @@ $(document).ready(function() {
   });
 
   function setOperation() {
+
+
     operation = $(this).val();
     if (firstNum === 0) {
       firstNum = temp;
@@ -118,16 +122,14 @@ $(document).ready(function() {
       storedOperator = operation;
     }
 
+
   }
+
 
   $('.imm-op').click(function() {
-    immOperation();
-  });
-
-  function immOperation() {
     operation = $(this).val();
     performOperation(firstNum, secondNum, operation);
-  }
+  });
 
   performOperation = function(a, b, operation_type) {
     let computationString = a + operation_type + b;
@@ -144,110 +146,3 @@ $(document).ready(function() {
   };
 
 });
-
-// $(document).ready(function() {
-
-//   let subtract, add, divide, multiply, calculate, percentage, clear, allClear, plusMinus, temp = '',
-//     firstNum = 0,
-//     secondNum = 0,
-//     total, displayValue, storedOperator, performOperation;
-
-//   let display = $('#display');
-
-//   subtract = function(num1, num2) {
-//     total = Number(num1) - Number(num2);
-//     return total;
-//   };
-
-//   add = function(num1, num2) {
-//     total = Number(num1) + Number(num2);
-//     return total;
-//   };
-
-//   multiply = function(num1, num2) {
-//     total = (Number(num1)) * (Number(num2));
-//     return total;
-//   };
-
-//   divide = function(num1, num2) {
-//     total = (Number(num1)) / (Number(num2));
-//     return total;
-//   };
-
-//   plusMinus = function() {
-//     if (total === 0) {
-//       total = Number(temp) * -1;
-//     }
-//     else {
-//       total = Number(total) * -1;
-//     }
-
-//   };
-
-//   clear = function() {
-//     total = 0;
-//     temp = 0;
-//     displayValue(total);
-//   };
-
-//   allClear = function() {
-//     total = 0;
-//     firstNum = 0;
-//     secondNum = 0;
-//     displayValue(total);
-//   };
-//   display.keypress(function() {
-
-//     if (temp === 0) {
-
-//       temp = '';
-//       addNum();
-//     }
-//     else {
-//       addNum();
-//     }
-//   });
-
-//   $('.int').click(function() {
-
-//     if (temp === 0) {
-//       temp = $(this).val().toString();
-//       displayValue(temp);
-//     }
-//     else {
-//       temp += $(this).val().toString();
-//       displayValue(temp);
-//     }
-//   });
-
-
-//   $('.operation').click(function() {
-
-//     operation = $(this).val();
-//     if (firstNum === 0) {
-//       firstNum = temp;
-//       displayValue(temp);
-//       temp = 0;
-//       storedOperator = operation;
-//     }
-//     else {
-//       secondNum = temp;
-//       performOperation(firstNum, secondNum, storedOperator);
-//       displayValue(total);
-//       storedOperator = operation;
-//     }
-//   });
-
-//   $('.imm-op').click(function() {
-//     operation = $(this).val();
-//     performOperation(firstNum, secondNum, operation);
-//   });
-
-
-
-//   displayValue = function(value) {
-//     display.val(value);
-
-//   };
-
-// });
